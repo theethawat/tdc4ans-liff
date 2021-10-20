e6868806db1b85f07f81d8d4d66976239e642e7f
Theethawat Savastham <theethawat@theduckcreator.in.th>
2021-10-20 23:03:38 +0700

    Build Frontend and Prepare Deploy

--- Backend/src/routers/static.js
+++ Backend/src/routers/static.js
@@ -1,8 +1,7 @@
 import express from "express";
+const path = require("path");
 const app = express.Router();
 
-app.get("/", (req, res) => {
-  res.send("The Duck Creator Express Server");
-});
+app.use("/", express.static(path.join(__dirname, "../public")));
 
 export default app;
